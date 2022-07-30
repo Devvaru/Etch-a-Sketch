551b29cd8dfbd5b2093af3bf1da550d866e05416
SyerAru <syeraru@gmail.com>
2022-07-29 22:03:17 -0600

    Write pseodocode for math to calculate grid size via button

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-let sixteen = 256;
+//to calculate grid size
 
+// let squared = (button selection) ;
+// let grid = (squared)
+// let num (pixel width and height ) = (square * 100) / grid;
+
+let sixteen = 256; //temp until math implemented
 const div = document.createElement("div");
 div.className = "pixel";
 const container = document.getElementById("container");
