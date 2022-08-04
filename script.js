2a6254b2437252f0dfc57a49d0f3fbd319026dcf
SyerAru <syeraru@gmail.com>
2022-08-04 02:42:35 -0600

    Add divs for layout and css

--- script.js
+++ script.js
@@ -5,23 +5,45 @@
 // let num (pixel width and height ) = (square * 100) / grid;
 
 const body = document.querySelectorAll('body');
+const background = document.getElementById('background');
+
+const container = document.createElement('div');
+container.id = "container";
+background.appendChild(container);
+
+const header = document.createElement('div');
+header.id = "header";
+container.appendChild(header);
+
+const title = document.createElement("h1");
+title.id = "title";
+title.textContent = "Etch-A-Sketch";
+header.appendChild(title);
+
+
+const main = document.createElement("div");
+main.id = "main"
+container.appendChild(main);
 
 const resetCanvas = document.createElement('button');
 resetCanvas.textContent = "Reset Canvas";
 resetCanvas.className = "button"
 resetCanvas.id = "resetButton"
-document.body.appendChild(resetCanvas);
+main.appendChild(resetCanvas);
+
+const canvas = document.createElement("div");
+canvas.id = "canvas";
+main.appendChild(canvas);
+
+const pixel = document.createElement("div");
+pixel.className = "pixel";
 
 let sixteen = 256; //temp until math implemented
 
-const container = document.getElementById("container");
-const div = document.createElement("div");
-div.className = "pixel";
-
-let num = prompt("Size of grid?")
-let squared = (num * 100 / (num * num))
-let gridSize = document.getElementByClassName(pixel).style.height = squared;
+// let num = prompt("Size of grid?")
+// let squared = (num * 100 / (num * num))
+// let gridSize = document.getElementByClassName(pixel).style.height = squared;
 
 for (let i = 0; i < sixteen; i++) {
- container.appendChild(div.cloneNode(true));
+ canvas.appendChild(pixel.cloneNode(true));
 }
