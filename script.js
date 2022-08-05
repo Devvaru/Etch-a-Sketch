8e020a975ae9da4ab1c99548d949d444ecf95728
SyerAru <syeraru@gmail.com>
2022-08-04 18:25:22 -0600

    Create columns for buttons and settings sections

--- script.js
+++ script.js
@@ -20,23 +20,41 @@ title.id = "title";
 title.textContent = "Etch-A-Sketch";
 header.appendChild(title);
 
-
 const main = document.createElement("div");
 main.id = "main"
 container.appendChild(main);
+
+//Buttons on the left (column)
+const buttonsLeft = document.createElement("div");
+buttonsLeft.id = "buttonsLeft";
+main.appendChild(buttonsLeft);
 
 const resetCanvas = document.createElement('button');
 resetCanvas.textContent = "Reset Canvas";
 resetCanvas.className = "button"
 resetCanvas.id = "resetButton"
-main.appendChild(resetCanvas);
+buttonsLeft.appendChild(resetCanvas);
 
+//Drawing area
 const canvas = document.createElement("div");
 canvas.id = "canvas";
 main.appendChild(canvas);
 
 const pixel = document.createElement("div");
 pixel.className = "pixel";
+
+//Buttons on the right (column)
+const buttonsRight = document.createElement("div");
+buttonsRight.id = "buttonsRight";
+main.appendChild(buttonsRight);
+
+const greyScale = document.createElement("button");
+greyScale.textContent = "Greyscale";
+greyScale.className = "button";
+greyScale.id  = "greyScaleButton";
+buttonsRight.appendChild(greyScale);
+
+
 
 let sixteen = 256; //temp until math implemented
 
