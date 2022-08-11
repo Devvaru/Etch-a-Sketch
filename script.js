070d8a03fd1bad99da4f6c24060d1ea8fdfcf01f
SyerAru <syeraru@gmail.com>
2022-08-10 21:22:42 -0600

    Reoder button declarations and appends

--- script.js
+++ script.js
@@ -27,31 +27,61 @@ container.appendChild(main);
 //Buttons on the left (column)
 const buttonsLeft = document.createElement("div");
 buttonsLeft.id = "buttonsLeft";
-main.appendChild(buttonsLeft);
 
 const defaultButton = document.createElement("button");
 defaultButton.textContent = "Default";
 defaultButton.className = "button";
 defaultButton.id = "defaultButton";
-buttonsLeft.appendChild(defaultButton);
 
 const grayscaleButton = document.createElement("button");
 grayscaleButton.textContent = "Grayscale";
 grayscaleButton.className = "button";
 grayscaleButton.id = "greyScaleButton";
-buttonsLeft.appendChild(grayscaleButton);
 
 const rainbowButton = document.createElement("button");
 rainbowButton.textContent = "Rainbow";
 rainbowButton.className = "button";
 rainbowButton.id = "rainbowButton";
-buttonsLeft.appendChild(rainbowButton);
 
 const scratchButton = document.createElement("button");
 scratchButton.textContent = "Scratch";
 scratchButton.className = "button";
 scratchButton.id = "scratch";
+
+//Append buttonsLeft
+main.appendChild(buttonsLeft);
+buttonsLeft.appendChild(defaultButton);
+buttonsLeft.appendChild(grayscaleButton);
+buttonsLeft.appendChild(rainbowButton);
 buttonsLeft.appendChild(scratchButton);
+
+
+//Buttons on the right (column)
+const buttonsRight = document.createElement("div");
+buttonsRight.id = "buttonsRight";
+
+const sliderContainer = document.createElement("div");
+sliderContainer.className = "sliderContainer";
+sliderContainer.id = "sliderContainer";
+
+//Slider slider.value()
+const slider = document.createElement("input");
+slider.setAttribute("type", "range");
+slider.min = "1";
+slider.max = "100";
+slider.value = "16";
+slider.className = "slider";
+slider.id = "slider";
+
+const eraseButton = document.createElement("button");
+eraseButton.textContent = "Eraser";
+eraseButton.className = "button";
+eraseButton.id = "eraseButton";
+
+const resetCanvas = document.createElement('button');
+resetCanvas.textContent = "Reset";
+resetCanvas.className = "button";
+resetCanvas.id = "resetButton";
 
 //Drawing area
 const canvas = document.createElement("div");
@@ -96,30 +126,19 @@ function createPixels() { //creates pixels in canvas
 
 createPixels()
 
+//Resets canvas by removing pixels as children
 function newCanvas() {
     canvas.replaceChildren();
 }
 
-//Buttons on the right (column)
-const buttonsRight = document.createElement("div");
-buttonsRight.id = "buttonsRight";
+//Append buttonsRight, declared earlier than canvas but appended after
 main.appendChild(buttonsRight);
-
-const sliderContainer = document.createElement("div");
-sliderContainer.className = "sliderContainer";
-sliderContainer.id = "sliderContainer";
 buttonsRight.appendChild(sliderContainer);
-
-//Slider slider.value()
-const slider = document.createElement("input");
-slider.setAttribute("type", "range");
-slider.min = "1";
-slider.max = "100";
-slider.value = "16";
-slider.className = "slider";
-slider.id = "slider";
 sliderContainer.appendChild(slider);
+buttonsRight.appendChild(eraseButton);
+buttonsRight.appendChild(resetCanvas);
 
+//Display numbers below slider
 const defaultValue = document.getElementById("slider").value;
 const sliderText = document.createElement("p");
 sliderText.id = "sliderText";
@@ -127,12 +146,13 @@ sliderText.className = "sliderText"
 sliderText.textContent = "Grid size: " + defaultValue + " x " + defaultValue;
 sliderContainer.appendChild(sliderText);
 
+//Slider listener
 slider.addEventListener("change", () => {
+
     const newValue = document.getElementById("slider").value;
     sliderText.textContent = "Grid size: " + newValue + " x " + newValue;
     sliderContainer.appendChild(sliderText);
     newCanvas();
-    
 
     //used to create pixels
     squared = (newValue * newValue)
@@ -150,22 +170,13 @@ slider.addEventListener("change", () => {
     createPixels();
 });
 
-const eraseButton = document.createElement("button");
-eraseButton.textContent = "Eraser";
-eraseButton.className = "button";
-eraseButton.id = "eraseButton";
-buttonsRight.appendChild(eraseButton);
-
-const resetCanvas = document.createElement('button');
-resetCanvas.textContent = "Reset";
-resetCanvas.className = "button";
-resetCanvas.id = "resetButton";
-buttonsRight.appendChild(resetCanvas);
-
+//Reset button listener
 resetCanvas.onclick = () => {
     newCanvas();
     createPixels();
 }
+
+
 
 
 // Drawing on the canvas
